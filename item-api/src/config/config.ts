1a94e4689011cb40aed9af453296436aa2a67485
Arshith Suresh <arshith007@gmail.com>
2026-01-12 12:38:41 +0530

    ENV Test

--- item-api/src/config/config.ts
+++ item-api/src/config/config.ts
@@ -8,8 +8,8 @@ interface Config {
 }
 
 const config: Config = {
-  port: Number(process.env.PORT) || 3000,
-  nodeEnv: process.env.NODE_ENV || "development",
+  port: Number(process.env.API_PORT) || 3000,
+  nodeEnv: process.env.API_ENV || "development",
 };
 
 export default config;
--- item-api/src/server.ts
+++ item-api/src/server.ts
@@ -2,5 +2,7 @@ import app from "./app";
 import config from "./config/config";
 
 app.listen(config.port, () => {
-  console.log(`Test server running on PORT : ${config.port}`);
+  console.log(
+    `Test server running on PORT : ${config.port} :: MODE ${config.nodeEnv}`
+  );
 });
